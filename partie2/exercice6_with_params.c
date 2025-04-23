dba404efef78393abb6343d2c48f8e346ded09cc
root <root@DESKTOP-P78MCK5>
2025-04-23 15:49:25 +0400

    2

--- partie2/exercice6_with_params.c
+++ partie2/exercice6_with_params.c
@@ -7,6 +7,28 @@
 
 #define NB_ARGUMENTS_MAX 10
 
+int decouperCommande(char *commande, char *arguments[]) {
+    int nbArguments = 0;
+    int i = 0;
+    while (commande[i] != '\0') {
+        if (commande[i] == ' ') {
+            commande[i] = '\0';
+        } else if (
+            (i == 0 || commande[i - 1] == '\0') &&
+            commande[i] != '\0' &&
+            nbArguments < NB_ARGUMENTS_MAX
+        ) {
+            arguments[nbArguments] = &commande[i];
+            nbArguments++;
+        }
+        i++;
+    }
+    arguments[nbArguments] = NULL;
+    return nbArguments;
+}
+
+
+
 int decouperCommande(char *commande, char *arguments[]);
 
 int main(int argc, char *argv[]) {
